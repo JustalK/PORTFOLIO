afc669bcc1a0fa21463e870ed958ad06d09c296d
Latsuj <justal.kevin@gmail.com>
2018-07-08 22:59:07 +0200

    Starting the switch between the page - Opening the windows effect

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,34 @@
 <template>
-    <img :src="image" />
+	<div id="HOME">
+		<div class="bloc left" :class={active:goPortfolio}>
+			<div>
+				<span id="TITLE">I'm Latsuj</span>
+	    	</div>
+	    </div>
+	    <div class="bloc right" :class={active:goPortfolio} ref="bloc-right">
+	    	<div>
+				<span id="TITLE">I'm Latsuj</span>
+			</div>	    
+	    </div>
+	    <a @click.stop="portfolio">Portfolio</a>
+	    <img :src="image" />
+	</div>
 </template>
 <script>
 import image from '../assets/imgs/campingcar.png'
 
 export default {
-    data: function () {
-        return {
-            image: image.preSrc
-        }
+    data: () => {
+    	return {
+			image: image.preSrc,
+			goPortfolio: false
+		}
+    },
+    methods: {
+    	portfolio: function() {
+    	 	console.log(this.goPortfolio);
+    		this.goPortfolio = true
+    	}
     }
 }
 </script>
