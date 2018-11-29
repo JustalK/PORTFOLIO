c805e7915dcc9ec843f11cf3a40b21b172751f78
Latsuj <justal.kevin@gmail.com>
2018-11-29 18:22:12 +0800

    adding the first intro sentance style

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,11 @@
 		<div class="wrap" :class={active:goZoom}>
 			<div class="bloc left" :class={active:goPortfolio}>
 				<div>
-					<span id="TITLE">- - - -</span>
+					<span class="intro">
+                       <span class="intro-first_sentance">Hello, I'm <a href="#" class="intro-name">Justal Kevin</a>.</span>
+                       <span class="intro-second_sentance">I'm a full stack web developer.</span>
+                       <span class="intro-third_sentance">Contact me at <a href="#" class="intro-email">justal.kevin@gmail.com</a></span>
+					</span>
 		    	</div>
 			    <a class="links-open-door left portfolio" @click.stop="open('portfolio')">
 			    	<div v-for="index in 20" :key="index" class="links-blocks" :class="['lb-' + (index*1-1)]"><div class="lb-inside">Liens 2</div></div>
@@ -14,14 +18,17 @@
 		    </div>
 		    <div class="bloc right" :class={active:goPortfolio} ref="bloc-right">
 		    	<div>
-					<span id="TITLE">- - - -</span>
+					<span class="intro">
+                       <span class="intro-first_sentance">Hello, I'm <a href="#" class="intro-name">Justal Kevin</a>.</span>
+                       <span class="intro-second_sentance">I'm a full stack web developer.</span>
+                       <span class="intro-third_sentance">Contact me at <a href="#" class="intro-email"><div class="intro-links_effect"></div>justal.kevin@gmail.com</a></span>
+					</span>
 				</div>	    
 			    <a class="links-open-door right articles" @click.stop="open('articles')">
 			    	<div v-for="index in 20" :key="index" class="links-blocks" :class="['lb-' + (index*1-1)]"><div class="lb-inside">Liens 2</div></div>
 			    	<div class="links-background">Liens 2</div>
 			    </a>
 		    </div>
-		    <span>Press START for playing</span>
 	    </div>
 	</div>
 </template>
@@ -31,7 +38,8 @@ export default {
     data: () => {
     	return {
 			goPortfolio: false,
-			goZoom: false
+			goZoom: false,
+			intro: "Hello, I'm Justal Kevin.<br>I'm a full stack web developer."
 		}
     },
     methods: {
