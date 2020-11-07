d6467a813605b96d2b0e5014e0908270b0db4606
Latsuj <justal.kevin@gmail.com>
2020-11-07 11:24:29 +0800

    [FEATURE] Adding the short description for all website

--- seeding/datas/articles/articles.js
+++ seeding/datas/articles/articles.js
@@ -6,7 +6,7 @@ module.exports = [
 	{
 		title: "Portfolio",
 		slug: "portfolio",
-		short_description: "A website for describing my work",
+		short_description: "A website for showing some of my work to the world.",
 		long_description: "laa laa laa laa laa laa laa laa laa laa laa laa laa laa laa laa laa laa laa laa laa laa laa laa laa laa",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5873"),
@@ -27,7 +27,7 @@ module.exports = [
     {
         title: "Altantic Grains App",
         slug: "atlantic-grains",
-        short_description: "An applications for managing trucks, stocks and deliveries",
+        short_description: "An applications for managing clients, sales, processes, trucks, stocks and deliveries.",
         long_description: "lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq lq",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5874"),
@@ -45,7 +45,7 @@ module.exports = [
     {
         title: "Predictive Insights",
 		slug: "predictive-insights",
-        short_description: "A web-app for managing the hiring process",
+        short_description: "A web-application for helping on the managing of employees and on the hiring process by using artificial intelligence.",
         long_description: "lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw lw",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5874"),
@@ -63,7 +63,7 @@ module.exports = [
 	{
 		title: "COVID19PH API",
 		slug: "covid19ph-api",
-		short_description: "An api for getting the latest cases in the Philippines",
+		short_description: "An REST API for getting all the informations about the cases in the Philippines by using the data from the csv of DOH.",
 		long_description: "lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5872")
@@ -74,7 +74,7 @@ module.exports = [
     {
         title: "Citiglobal Nudge",
 		slug: "citiglobal-nudge",
-        short_description: "A backend app for managing the performance of their employee",
+        short_description: "A backend app for keeping the whole company updated about the work of their employees and managers.",
         long_description: "le le le le le le le le le le le le le le le le le le le le le le le le le le le le le le le le le le le le le le le le le le le",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5877"),
@@ -89,7 +89,7 @@ module.exports = [
     {
         title: "Bounty AI",
 		slug: "bounty-ai",
-        short_description: "A web-app for finding the best location for malls",
+        short_description: "A web-app for predicting the best location for building a futur malls by using artificial intelligence.",
         long_description: "lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5874"),
@@ -106,7 +106,7 @@ module.exports = [
     {
         title: "Labonapp",
 		slug: "labonapp",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A web-app for discovering the restaurant around you, getting the latest promotions and making reservations.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5880"),
@@ -123,7 +123,7 @@ module.exports = [
     {
         title: "Happee",
 		slug: "happee",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A website for renting a motorvan to a private individual, discovering new destinations and getting informations about travelling in motorvan.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5880"),
@@ -140,7 +140,7 @@ module.exports = [
     {
         title: "ID Newsletter",
 		slug: "id-newsletter",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A website for sending, programming and designing newsletter with a very simple UI.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5880"),
@@ -155,7 +155,7 @@ module.exports = [
     {
         title: "Promarine",
 		slug: "promarine",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A website for showing the latest news of promarine and selling boats.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5880"),
@@ -170,7 +170,7 @@ module.exports = [
     {
         title: "Saona Villas",
 		slug: "saona-villas",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A multilanguage website for renting beautiful villas in different countries accross the world.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5880"),
@@ -185,7 +185,7 @@ module.exports = [
     {
         title: "Maison Lucas",
 		slug: "maison-lucas",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A website for selling premium sea food and ordering special sea food meal from a professional chef.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5875"),
@@ -200,7 +200,7 @@ module.exports = [
     {
         title: "Gouter Magique",
 		slug: "gouter-magique",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A website for showing the latest delicious cake from the famous Wahou factory.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5880"),
@@ -213,9 +213,9 @@ module.exports = [
         order: 10
     },
     {
-        title: "Foire Au Vin",
-		slug: "foire-au-vin",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        title: "Le gout du vin",
+		slug: "le-gout-du-vin-56",
+        short_description: "A website for giving all the latest news and events about wines and some alcohol in Britany.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5880"),
@@ -230,7 +230,7 @@ module.exports = [
     {
         title: "Finister Assurance",
 		slug: "finistere assurance",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "The official website of the Finister Assurance insurrance based in Britany.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5880"),
@@ -245,7 +245,7 @@ module.exports = [
     {
         title: "Sails Concept",
 		slug: "sails-concept",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A website for presenting boats of Sails Concepts and for creating and designing your custom sails.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5880"),
@@ -260,7 +260,7 @@ module.exports = [
     {
         title: "David Paysages",
 		slug: "david-paysages",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A website for presenting the work of the landscaper and artist David Paysage.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5880"),
@@ -275,7 +275,7 @@ module.exports = [
     {
         title: "EMD Pro",
 		slug: "emd-pro",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A website for selling professional equipements for armies. No weapons are sold on this website !",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5875"),
@@ -289,7 +289,7 @@ module.exports = [
     {
         title: "Zipworld",
 		slug: "zipworld",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A website for presenting the tours of Zipworld and booking flights at the lowest price possible.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5889"),
@@ -305,7 +305,7 @@ module.exports = [
     {
         title: "Onarto",
 		slug: "onarto",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A website for selling Thai painting and scultures, presenting the best events in Bangkok involving arts and giving informations about arts in general.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5875"),
@@ -321,7 +321,7 @@ module.exports = [
     {
         title: "Odyssea",
 		slug: "odyssea",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "The official website for the association Odyssea who create events and raise money for the research against the breast cancer.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5875"),
@@ -337,7 +337,7 @@ module.exports = [
     {
         title: "Le monde de Zip",
 		slug: "le-monde-de-zip",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A website giving a lot of advises about what are the countries to visit or those who are to be avoid.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5889"),
@@ -353,7 +353,7 @@ module.exports = [
     {
         title: "El Mercado",
 		slug: "el-mercado",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A website for selling imported food from Europe in Thailand.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5875"),
@@ -369,7 +369,7 @@ module.exports = [
     {
         title: "Ogocare",
 		slug: "ogocare",
-        short_description: "A web-app for making a reservation to a restaurant in Britany",
+        short_description: "A website for finding and getting informations about the best hospital in Thailand around you.",
         long_description: "ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly ly",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5889"),
@@ -385,7 +385,7 @@ module.exports = [
 	{
 		title: "Pornhub API",
 		slug: "pornhub-api",
-		short_description: "An api for scrapping the infamous website",
+		short_description: "A REST API for scrapping the famous porn website"
 		long_description: "lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5872")
@@ -396,7 +396,7 @@ module.exports = [
 	{
 		title: "Anime API",
 		slug: "anime-api",
-		short_description: "An api for finding streaming or downloading links for animes",
+		short_description: "A REST API for finding streaming or downloading links for animes on multiple website instantly.",
 		long_description: "lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr lr",
 		tags: [
 			mongoose.Types.ObjectId("5f95461688489acdd8ee5872")
