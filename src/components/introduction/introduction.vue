f5a8d56e6625cc1971d3ff5b3f2b553271ee0bdc
Latsuj <justal.kevin@gmail.com>
2020-11-17 01:25:40 +0800

    [FEATURE] Adding a type effect on the home

--- src/components/introduction/introduction.vue
+++ src/components/introduction/introduction.vue
@@ -3,10 +3,9 @@
 		<span class="intro">
 			<span>Hello world, I'm <a
 				class="capitalize"
-				href="#">{{ props_introduction.name }}</a>.</span>
-			<span>I'm a full stack web developer.</span>
-			<span>If any questions, contact me at <a
-				href="#">{{ props_introduction.email }}</a></span>
+				:href="&quot;mailto:&quot; + props_introduction.email">{{ props_introduction.name }}</a>.</span>
+			<span>I'm a <em>{{ job }}</em>.</span>
+			<span>If any questions, contact me at <a :href="&quot;mailto:&quot; + props_introduction.email">{{ props_introduction.email }}</a></span>
 		</span>
 	</div>
 </template>
@@ -16,6 +15,34 @@ export default {
 		props_introduction: {
 			type: Object,
 			required: true
+		}
+	},
+	data: () => {
+		return {
+			jobs: ['Full stack developer', 'Web Developer', 'Frontend Developer', 'Backend Developer', 'System Administrator'],
+			job_index: 0,
+			job: ''
+		};
+	},
+	watch: {
+		props_introduction() {
+			this.writing();
+		},
+	},
+	methods: {
+		writing() {
+			const i = this.job.length;
+			if (i < this.jobs[this.job_index].length) {
+				this.job += this.jobs[this.job_index].charAt(i);
+				setTimeout(this.writing, 50);
+			} else {
+				setTimeout(this.switch_job, 2000);
+			}
+		},
+		switch_job() {
+			this.job_index = (this.job_index + 1) % this.jobs.length;
+			this.job = '';
+			this.writing();
 		}
 	}
 };
