4feee8590e6acf8d9cca44b807b0ffbdf8671e4d
Latsuj <justal.kevin@gmail.com>
2021-03-04 20:02:58 +0800

    [FEATURE] Adding a page privacy for the app

--- seeding/datas/pages/pages.js
+++ seeding/datas/pages/pages.js
@@ -4,13 +4,42 @@ const mongoose = require('mongoose');
 
 module.exports = [
 	{
-		name: "portfolio",
-		title: "Work",
-		description: "From Web Components and UI/UX animations to React.JS, Vue.JS and Node.JS, I have been working in many different fields and countries ! Check out my latest projects and work."
+		name: 'portfolio',
+		title: 'Work',
+		description: 'From Web Components and UI/UX animations to React.JS, Vue.JS and Node.JS, I have been working in many different fields and countries ! Check out my latest projects and work.'
 	},
 	{
-		name: "resume",
-		title: "My resume",
-		description: "Under you will find my resume updated from github with my current and previous jobs, informations about some of my tasks and the technologies I am really confortable with."
+		name: 'resume',
+		title: 'My resume',
+		description: 'Under you will find my resume updated from github with my current and previous jobs, informations about some of my tasks and the technologies I am really confortable with.'
+	},
+	{
+		name: 'privacy',
+		title: 'Privacy Policy',
+		description: `<p>Effective as of January 25, 2019</p>
+
+    <h2>Introduction</h2>
+
+    <p>At Justalk, we collect, use and share personal data in accordance with privacy laws to make sure your data protection rights are implemented and enforced. We integrate various technical solutions to comply with applicable laws and rules related to personal data protection in countries where we operate. This Privacy Policy sets forth the basic rules and principles by which we process your personal data, and mentions our responsibilities while processing personal data.</p>
+
+    <p>We may be a data processor or data controller according to the applicable law. Regardless of our status, we will deal with any personal data as required by any applicable regulation, including but not limited to, Regulation (EU) 2016/679 of the European Parliament and of the Council of 27 April 2016 on the protection of natural persons with regard to the processing of personal data and on the free movement of such data, and repealing Directive 95/46/EC (the “GDPR”).</p>
+
+    <p>We do not knowingly attempt to solicit or receive information from children. Our services do not aim at children. The concrete age of the children are defined differently, so any case involving a child will be reviewed individually.</p>
+
+    <p>We understand that you are aware of and care about your own privacy interests, and we take that seriously. This Privacy Policy describes the rules and practices with regard to the collection and use of your personal data and details your privacy rights. We recognize that privacy is an ongoing responsibility, and so we will from time to time update this Privacy Policy as we undertake new personal data practices or adopt new privacy and security rules.</p>
+
+    <p>Our data protection officer is Justal Kevin, Address: 3706 Acqua Private Residence, Manila, Philippines, email: justal.kevin@gmail.com</p>
+
+    <h2>1. TERMS WE USE AS LEGAL GUIDELINES OF THE PROCESSING</h2>
+
+    <p>There are some legal bases for the processing of your personal data and we count on them to process your personal data. We use the main three bases to process your personal data: consent, contract, and legitimate interest.</p>
+
+    <p>Consent - your clear agreement to the processing of your personal data for a specific purpose.</p>
+
+    <p>Contract - the reason why the processing is necessary based on a contract you have with us, or because we have asked you to take specific steps before entering into that contract.</p>
+
+    <p>Legitimate Interests - the reason why the processing your data is necessary which is based on the legitimate interests or the legitimate interests of a third party, provided those interests are not outweighed by your rights and interests.</p>
+
+    <p>Articles 6(1) and 9(2) of the GDPR also indicate other legal grounds for the processing and when applicable we will count on such grounds.</p>`
 	}
-]
+];
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import app from './pages/app';
 import home from './pages/home';
 import resume from './pages/resume';
+import privacy from './pages/privacy';
 import portfolio from './pages/portfolio';
 import project from './pages/project';
 import page_not_found from './pages/page_not_found';
@@ -21,6 +22,7 @@ const routes = [
 	{ path: '/portfolio', name: 'portfolio', component: portfolio },
 	{ path: '/portfolio/:slug', name: 'project', component: project },
 	{ path: '/resume', name: 'resume', component: resume },
+	{ path: '/privacy', name: 'privacy', component: privacy },
 	{ path: '*', component: page_not_found }
 ];
 
