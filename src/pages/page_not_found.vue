ac14e578bef31699d2b993eff3bad7588a6d62c5
Latsuj <justal.kevin@gmail.com>
2021-04-03 13:15:14 +0800

    [FEATURE] Adding design link to home 404

--- src/pages/page_not_found.vue
+++ src/pages/page_not_found.vue
@@ -3,10 +3,29 @@
 		<div>
 			<h1>404</h1>
 			<span>Sorry, this page does not exist...</span>
+			<a
+				href="#"
+				@click.stop="back">
+				<svg>
+					<rect />
+				</svg>
+				Back to home
+			</a>
 		</div>
 	</div>
 </template>
 <script>
+import utils from '../helper/utils.js';
 
+export default {
+	methods: {
+		back() {
+			utils.add_class_to_element(this.$refs.portfolio, 'unmounted');
+			setTimeout(() => {
+				this.$router.push({name: 'home'});
+			},1000);
+		},
+	}
+};
 </script>
 <style src="../assets/less/page_not_found.less"></style>
