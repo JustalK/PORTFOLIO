71cb1114b06d934119d76454e07261d7dc2956f8
Latsuj <justal.kevin@gmail.com>
2021-04-04 20:43:33 +0800

    [FEATURE] Adding the first element inside the canvas of project

--- src/components/slide.vue
+++ src/components/slide.vue
@@ -66,6 +66,8 @@
 import utils from '../helper/utils.js';
 import * as THREE from '../libs/three.js';
 
+const extrudeSettings = { amount: 10, bevelEnabled: true, bevelSegments: 1, steps: 2, bevelSize: 3, bevelThickness: 3 };
+
 
 export default {
 	props: {
@@ -98,7 +100,7 @@ export default {
 	methods: {
 		initCamera() {
 			this.camera = new THREE.PerspectiveCamera(70, this.$refs.canvas.clientWidth / this.$refs.canvas.clientHeight);
-			this.camera.position.z = 50;
+			this.camera.position.z = 200;
 
 			this.scene = new THREE.Scene();
 
@@ -108,11 +110,57 @@ export default {
 			this.scene.add(cube);
 			cube.rotation.set(0.4, 0.2, 0);
 
+			this.scene.add(this.createBoard());
+
 			this.renderer = new THREE.WebGLRenderer( { antialias: true } );
 			this.renderer.setPixelRatio( window.devicePixelRatio );
-			this.renderer.setClearColor(0xDDDDDD, 1);
+			this.renderer.setClearColor(0x111116, 1);
 			this.renderer.setSize( this.$refs.canvas.clientWidth, this.$refs.canvas.clientHeight );
 			this.$refs.canvas.appendChild( this.renderer.domElement );
+		},
+		createBoard() {
+			const boardTmp = new THREE.Group();
+
+			// Construct the mesh piece by piece
+			const piece = [];
+			piece.push(this.createSideWireframe(0,0,0,0,0,0));
+
+			// Add the differents parts to the group of meshes
+			for(var i=piece.length;i--;) {
+				boardTmp.add(piece[i]);
+			}
+
+			boardTmp.position.set(-150, 0, 0);
+			boardTmp.rotation.set(0, 0, 0);
+
+			return boardTmp;
+		},
+		createShape() {
+			const leftShape = new THREE.Shape();
+			leftShape.moveTo( -5, -40 );
+			leftShape.lineTo( 0, 15 );
+			leftShape.lineTo( 10, 25 );
+			leftShape.lineTo( 10, 70 );
+			leftShape.lineTo( 0, 80 );
+			leftShape.lineTo( -5, 130 );
+			leftShape.lineTo( 40, 120 );
+			leftShape.lineTo( 45, 115 );
+			leftShape.lineTo( 25, 115 );
+			leftShape.lineTo( 20, 110 );
+			leftShape.lineTo( 20, -10 );
+			leftShape.lineTo( 25, -15 );
+			leftShape.lineTo( 45, -15 );
+			leftShape.lineTo( 40, -20 );
+			return leftShape;
+		},
+		createSideWireframe(x,y,z,rx,ry,rz) {
+			const geometryBoard = new THREE.ExtrudeGeometry( this.createShape(), extrudeSettings );
+			const material = new THREE.LineBasicMaterial( { color: 0xFFFFFF, linewidth: 1 } );
+			const sideWireframe = new THREE.LineSegments( new THREE.EdgesGeometry( geometryBoard ), material );
+			sideWireframe['wireframe'] = true;
+			sideWireframe.position.set( x, y, z );
+			sideWireframe.rotation.set( rx, ry, rz );
+			return sideWireframe;
 		},
 		animate() {
 			requestAnimationFrame( this.animate );
